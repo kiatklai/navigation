73d5d24a674dc92559091c81290529d0a1641786
kiatklai <likhitthaworn.k@gmail.com>
2023-08-02 21:58:02 +0900

    navber表示非表示できるように

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,16 +1,39 @@
 import {Link} from "react-router-dom"
-import { FaBars } from "react-icons/fa";
+import { FaBars,FaHome,FaWindowClose } from "react-icons/fa";
+import { useState } from "react";
+import './Navigation.css'
 
 const Navigation=()=>{
+  const [showMenu,setShowMenu] = useState(false)
+  const toggleMenu=()=>setShowMenu(!showMenu)
+
   return(
     <aside>
       <div className="navbar">
         <div className="navbar-toggle">
           <Link to="#" className="menu-bar">
-            <FaBars/>
+            <FaBars onClick={toggleMenu}/>
           </Link>
         </div>
       </div>
+      <nav className={showMenu ? "nav-menu active" : "nav-menu"}>
+        <ul className="nav-menu-item">
+          <li className="navbar-toggle">
+            <Link to="#" className="menu-bar">
+              <FaWindowClose onClick={toggleMenu}/>
+            </Link>
+          </li>
+          <li className="menu-text">
+            <Link to=""><FaHome/><span>First Page</span></Link>
+          </li>
+          <li className="menu-text">
+            <Link to=""><FaHome/><span>First Page</span></Link>
+          </li>
+          <li className="menu-text">
+            <Link to=""><FaHome/><span>First Page</span></Link>
+          </li>
+        </ul>
+      </nav>
     </aside>
   )
 }
